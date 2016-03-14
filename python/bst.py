5f5ebb4bdea1ab4cbb338f2c5ec25566d62521ee
Alvin <alvin.zablan62@myhunter.cuny.edu>
2016-03-14 14:27:57 -0400

    optimized path sum

--- python/bst.py
+++ python/bst.py
@@ -42,39 +42,44 @@ class BST:
 tree = BST()
 tree.insert(50)
 tree.insert(25)
+tree.insert(20)
+tree.insert(30)
 tree.insert(75)
-tree.insert(15)
-tree.insert(35)
-tree.insert(65)
-tree.insert(85)
-tree.insert(100)
-tree.insert(0)
+tree.insert(70)
+tree.insert(80)
 
 # tree.printTree()
 
-def pathSum(root, n, target, count):
-	if n == target:
-		count[0] += 1
-	if root.left is not None:
-		pathSum(root.left, n + root.left.data, target, count)
-	if root.right is not None:
-		pathSum(root.right, n + root.right.data, target, count)
+def countPathSum(root, target):
+	count = 0
+	sums = {}
+	countPath(root, target, sums)
+	for l in sums.values():
+		print l
+		for s in l:
+			if s == target:
+				count += 1
+	return count
 
-def countPath(root, target, count):
+def pathSum(root, target, sums):
+	if root.left is None and root.right is None:
+		sums[id(root)] = [root.data]
+		return
+	if id(root) not in sums:
+		sums[id(root)] = [root.data]
+	if root.left is not None:
+		sums[id(root)].extend([(root.data + s) for s in sums[id(root.left)]])
+	if root.right is not None:
+		sums[id(root)].extend([(root.data + s) for s in sums[id(root.right)]])
+
+def countPath(root, target, sums):
 	if root is None:
 		return
-	else:
-		pathSum(root, root.data, target, count)
-		countPath(root.left, target, count)
-		countPath(root.right, target, count)
-
-def countPathSum(root, target):
-	count = [0]
-	countPath(root, target, count)
-	return count[0]
+	countPath(root.left, target, sums)
+	countPath(root.right, target, sums)
+	pathSum(root, target, sums)
 
 print countPathSum(tree.root, 75)
-
 
 
 
--- python/pathSum.py
+++ python/pathSum.py
@@ -1,20 +1,53 @@
-def pathSum(root, n, target, count):
-	if n == target:
-		count[0] += 1
-	if root.left is not None:
-		pathSum(root.left, n + root.left.data, target, count)
-	if root.right is not None:
-		pathSum(root.right, n + root.right.data, target, count)
+# BRUTE FORCE SOLUTION O(N^2) TIME:
+# def pathSum(root, n, target, count):
+# 	if n == target:
+# 		count[0] += 1
+# 	if root.left is not None:
+# 		pathSum(root.left, n + root.left.data, target, count)
+# 	if root.right is not None:
+# 		pathSum(root.right, n + root.right.data, target, count)
 
-def countPath(root, target, count):
-	if root is None:
-		return
-	else:
-		pathSum(root, root.data, target, count)
-		countPath(root.left, target, count)
-		countPath(root.right, target, count)
+# def countPath(root, target, count):
+# 	if root is None:
+# 		return
+# 	else:
+# 		pathSum(root, root.data, target, count)
+# 		countPath(root.left, target, count)
+# 		countPath(root.right, target, count)
+
+# def countPathSum(root, target):
+# 	count = [0]
+# 	countPath(root, target, count)
+# 	return count[0]
 
 def countPathSum(root, target):
-	count = [0]
-	countPath(root, target, count)
-	return count[0]
+	count = 0
+	sums = {}
+	countPath(root, target, sums)
+	for l in sums.values():
+		for s in l:
+			if s == target:
+				count += 1
+	return count
+
+def pathSum(root, target, sums):
+	if root.left is None and root.right is None:
+		sums[id(root)] = [0, root.data]
+		return
+	if id(root) not in sums:
+		sums[id(root)] = [0]
+	if root.left is not None:
+		sums[id(root)].extend([(root.data + s) for s in sums[id(root.left)]])
+	if root.right is not None:
+		sums[id(root)].extend([(root.data + s) for s in sums[id(root.right)]])
+
+def countPath(root, target, sums):
+	if root is None:
+		return
+	countPath(root.left, target, sums)
+	countPath(root.right, target, sums)
+	pathSum(root, target, sums)
+
+
+
+
