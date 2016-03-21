8eb751b410647f9face950ad3bace7a44ec49a40
Alvin Zablan <azablan.dev@gmail.com>
2016-03-21 18:25:23 -0400

    binary bunnies

--- foobar/binaryBunnies.py
+++ foobar/binaryBunnies.py
@@ -1,5 +1,3 @@
-from random import shuffle
-from time import time
 from math import factorial
 
 class Node():
@@ -11,24 +9,19 @@ class Node():
 class Tree():
 	def __init__(self):
 		self.root = None
-		self.childMap = {}
 
 	def insert(self, node, n):
-		if n not in self.childMap.keys():
-			self.childMap[n] = set()
 		if self.root is None:
 			self.root = Node(n)
 			return
 		if n < node.data:
 			if node.right is None:
 				node.right = Node(n)
-				self.childMap[node.data].add(n)
 			else:
 				self.insert(node.right, n)
 		elif n > node.data:
 			if node.left is None:
 				node.left = Node(n)
-				self.childMap[node.data].add(n)
 			else:
 				self.insert(node.left, n)
 
@@ -36,38 +29,10 @@ class Tree():
 		while l:
 			self.insert(self.root, l.pop(0))
 
-	def printTree(self, node):
-		if node is None:
-			return
-		else:
-			self.printTree(node.left)
-			print node.data
-			self.printTree(node.right)
-
-def permute(l, frontier, numChildren, childMap, count):
-	if len(l) == len(childMap):
-		count[0] += 1
-		return
-	newFrontier = set()
-	for parent in frontier:
-		if numChildren[parent] < 2:
-			newFrontier.add(parent)
-	for parent in newFrontier:
-		for c in childMap[parent]:
-			if c not in l:
-				thisL = l.copy()
-				thisL.add(c)
-				thisFrontier = newFrontier.copy()
-				thisFrontier.add(c)
-				thisNumChildren = numChildren.copy()
-				thisNumChildren[parent] += 1
-				permute(thisL, thisFrontier, thisNumChildren, childMap, count)
-
-
 def countBuilds(root):
 	if root is None:
-		return 0
-	return weave(countBuilds(root.left) * size(root.left), countBuilds(root.right) * size(root.right))
+		return 1
+	return weave(size(root.left), size(root.right)) * countBuilds(root.left) * countBuilds(root.right)
 
 def size(root):
 	if root is None:
@@ -80,29 +45,4 @@ def weave(a, b):
 def answer(seq):
 	tree = Tree()
 	tree.buildTree(seq[:])
-	# print size(tree.root)
-	print countBuilds(tree.root)
-
-answer([10, 5, 6, 4, 15])
-answer([10, 5, 15])
-answer([10, 5,6, 15])
-answer([5,9,8,2,1])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	return str(countBuilds(tree.root))
