ac138c55d3874c8f25b7c54443440b22f5bc29e9
Alvin Zablan <azablan.dev@gmail.com>
2016-04-02 23:22:22 -0400

    subsort

---
+++ python/subSort.py
@@ -0,0 +1,45 @@
+
+
+def subSortSlow(a):
+	asorted = sorted(a)
+	for i in range(0, len(a)):
+		if a[i] != asorted[i]:
+			left = i
+			break
+	for j in range(len(a) - 1, -1, -1):
+		if a[j] != asorted[j]:
+			right = j
+			break
+	return (left, right)
+# O(nlogn)
+
+
+def subSort(a):
+	for i in range(0, len(a) - 1):
+		if a[i] > a[i + 1]:
+			left = i
+			break
+	for j in range(len(a) - 1, 0, -1):
+		if a[j] < a[j - 1]:
+			right = j
+			break
+	minL = min(a[left:right + 1])
+	maxR = max(a[left:right + 1])
+	for i in range(left - 1, -1, -1):
+		if a[i] < minL:
+			left = i + 1
+			break
+	for j in range(right + 1, len(a)):
+		if a[j] > maxR:
+			right = j - 1
+			break
+	return (left, right)
+# O(n)
+
+a = [6,1,2,3,4,5,7,8,9]
+print subSort(a)
+print subSortBrute(a)
+
+
+
+
