cd7a3cc4ada10651a8763ef36fe46ab0e47a5c87
Alvin Zablan <azablan.dev@gmail.com>
2016-04-13 16:55:48 -0400

    finish sort matrix

--- python/sortMatrix.py
+++ python/sortMatrix.py
@@ -1,7 +1,13 @@
-# matrix such that an element is >= the element to the right and the element below
-def sortMatrix(m):
+from heapq import heappush, heappop
+from time import time
+
+# matrix such that an element is >= the element to the right
+# and the element below
+
+# O(nk*lognk) runtime
+def sortMatrixSlow(m):
 	if len(m) < len(m[0]):
-		return mergeLists(m)
+		return mergeLists()
 	else:
 		newMatrix = []
 		for i in range(0, len(m[0])):
@@ -12,10 +18,11 @@ def sortMatrix(m):
 		return mergeLists(newMatrix)
 
 def mergeLists(lists):
+	lists = lists[:]
 	result = []
 	for l in lists:
 		result = merge(l, result)
-	return result
+	return result[::-1]
 
 def merge(alist, blist):
 	ans = []
@@ -32,13 +39,42 @@ def merge(alist, blist):
 			ans.append(b.pop(0))
 	return ans
 
+# for k by n matrix O(kn*log(k)) runtime if k < n
+def sortMatrixFast(m):
+	if len(m) < len(m[0]):
+		return mergeListsQ()
+	else:
+		newMatrix = []
+		for i in range(0, len(m[0])):
+			newL = []
+			for l in m:
+				newL.append(l[i])		
+			newMatrix.append(newL)
+		return mergeListsQ(newMatrix)
+
+def mergeListsQ(lists):
+	lists = lists[:]
+	h = []
+	for i in range(0, len(lists)):
+		pair = (lists[i].pop(), i)
+		heappush(h, pair)
+	ans = []
+	while h:
+		value, i = heappop(h)
+		ans.append(value)
+		if lists[i]:
+			pair = (lists[i].pop(), i)
+			heappush(h, pair)
+	return ans
+
 
 m = [
-	[100, 90, 80, 70],
-	[92, 90, 77, 59],
-	[69, 68, 71, 56],
-	[64, 30, 29, 29],
-	[27, 24, 22, 21],
+	[100, 90, 80, 70, 68, 50],
+	[92, 90, 77, 59, 55, 33],
+	[69, 68, 71, 56, 54, 33],
+	[64, 30, 29, 29, 22, 16],
+	[27, 24, 22, 21, 13, 11],
+	[23, 21, 18, 16, 11, 10]
 ]
 
-print sortMatrix(m)
+print sortMatrixFast(m)
