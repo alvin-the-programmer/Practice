dffe641709b6785c9f824c2e8ecf8dbfd70ae60b
Alvin Zablan <azablan.dev@gmail.com>
2016-04-16 20:48:28 -0400

    finish zero matrix optimally

--- python/zeroMatrix.py
+++ python/zeroMatrix.py
@@ -1,23 +1,38 @@
 import numpy
 
+# O(nm) time, O(1) space
 def zeroMatrix(m):
 	print m
-	rows, cols = findZeros(m)
-	for r in rows:
-		zeroRow(r, m)
-	for c in cols:
-		zeroCol(c, m)
+	zeroFirstRow, zeroFirstCol = findZeros(m)
+	for r in range(1, len(m)):
+		if m[r][0] == -1:
+			zeroRow(r, m)
+	for c in range(1, len(m[0])):
+		if m[0][c] == -1:
+			zeroCol(c, m)
+	if zeroFirstRow:
+		zeroRow(0, m)
+	if zeroFirstCol:
+		zeroCol(0, m)
 	print m
 
 def findZeros(m):
-	zeroRows = set()
-	zeroCols = set()
+	zeroRow = False
+	zeroCol = False
+	for r in range(0, len(m)):
+		if m[r][0] == 0:
+			zeroCol = True
+			break
+	for c in range(0, len(m[0])):
+		if m[0][c] == 0:
+			zeroRow = True
+			break
 	for row in range(0, len(m)):
 		for col in range(0, len(m[0])):
 			if m[row][col] == 0:
-				zeroRows.add(row)
-				zeroCols.add(col)
-	return zeroRows, zeroCols
+				m[0][col] = -1
+				m[row][0] = -1
+	return zeroRow, zeroCol
 
 def zeroRow(row, m):
 	for i in range(0, len(m[0])):
@@ -30,7 +45,7 @@ def zeroCol(col, m):
 m = numpy.random.random_integers(1,9, (9,9))
 m[0][1] = 0
 m[3][7] = 0
+m[3][0] = 0
 m[8][8] = 0
 m[5][1] = 0
-
 zeroMatrix(m)
